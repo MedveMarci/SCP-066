b7c8d689af9b933ecc22ad07f703c0b3defa29a9
RisottoMan <lapka.roman@yandex.ru>
2025-07-27 13:08:59 +0500

    Small refactoring

--- Scp066/Plugin.cs
+++ Scp066/Plugin.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using Exiled.API.Features;
 using Exiled.CustomRoles.API;
 using Scp066.Configs;
@@ -16,27 +15,16 @@ public class Plugin : Plugin<Config>
     public override void OnEnabled()
     {
         Singleton = this;
-        new EventHandler();
-
-        // Checking that the ProjectMER plugin is loaded on the server
-        if (!AppDomain.CurrentDomain.GetAssemblies().Any(x => x.FullName.ToLower().Contains("projectmer")))
-        {
-            Log.Error("ProjectMER is not installed. Schematics can't spawn the game model.");
+        
+        // Setup the RoleAPI
+        if (!RoleAPI.Startup.SetupAPI(this.Name))
             return;
-        }
         
         // Register the custom role
         Config.Scp066RoleConfig.Register();
         
-        // Setup the RoleAPI
-        RoleAPI.Startup.SetupAPI(this.Name);
+        new EventHandler();
         
-        // Register the abilities
-        RoleAPI.API.Managers.AbilityRegistrator.RegisterAbilities();
-        RoleAPI.API.Managers.KeybindManager.RegisterKeybinds(
-            RoleAPI.API.Managers.AbilityRegistrator.GetAbilities,
-            "SCP-066");
-
         base.OnEnabled();
     }
 }
